7f1adbc7e854017cba1a03acdffe95cae519eb5b
Aimane Jaghda <aimane.jaghda@gmail.com>
2024-05-08 18:26:38 +0100

    designing login page

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -3,7 +3,21 @@ import React from 'react'
 export default function Signup() {
   return (
     <div>
-      <h1 className="mt-5 text-center h1">Signup</h1>
+      <div className="shadow-lg rounded-4 signupForm">
+        <div className="brand-logo display-3 text-center my-3">ABS</div>
+        <form action="">
+          <div className="mt-5 row">
+            <div className="col-12 col-md-6">
+              <label htmlFor="lastName" className="form-label">Nom</label>
+              <input type="text" name="lastName" id="lastName" className="form-control" />
+            </div>
+            <div className="col-12 col-md-6">
+              <label htmlFor="lastName" className="form-label">Nom</label>
+              <input type="text" name="lastName" id="lastName" className="form-control" />
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }

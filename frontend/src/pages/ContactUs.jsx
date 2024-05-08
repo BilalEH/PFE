dcf4d3639ff6cbdc7740c992e68e32eee6bfcce7
saad <saad16dirassa@gmail.com>
2024-05-09 00:06:58 +0100

    login v1

--- frontend/src/pages/ContactUs.jsx
+++ frontend/src/pages/ContactUs.jsx
@@ -5,12 +5,12 @@ function ContactUs() {
   return (
 
     <div className=' '>
-        <div className=' container my-5 d-flex justify-content-space-around '>
+        <div className=' container my-5 d-flex justify-content-space-around  '>
 
         <div className='left_side ' >
 
            <h1 className='display-6 ms-3 mt-3 text-success bold' >Contact us</h1>
- 
+
         </div>
 
         <div className='  v2 right_side'>
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,67 +1,46 @@
-import React from 'react'
-import "./login.css"
+import React, { useState } from "react";
+import "./login.css";
 import { Link } from 'react-router-dom';
 
-
-
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("login attempt");
 
-
-
-
-
-  function handleSubmit(e){
-  e.preventDefault();
-  console.log("login attempt");
-}
-
-
-
-
-
-
-
-
-  function handleSubmit(e){
-  e.preventDefault();
-  console.log("login attempt");
-}
-
+    // You can access email and password here directly
+    console.log("Email:", email);
+    console.log("Password:", password);
+  }
 
   return (
     <div>
       <div className="shadow-lg rounded-4 loginform">
-        <div className="brand-logo text-center my-3"><span className='display-3'>A
-          <span className='logoB'>
-            B
-          </span>
-          <span className='logoS'>
-            S
-          </span>
-          </span>
-          center</div>
+        <div className="brand-logo text-center my-3">
+          <span className='display-3'>AB</span> center
+        </div>
         <form action="" className='mt-5'>
           <div className="my-3">
-              <label htmlFor="email" className="form-label">Email</label>
-              <input type="text" name="email" id="email" className="form-control" />
+            <label htmlFor="email" className="form-label">Email</label>
+            <input type="text" name="email" id="email" className="form-control" onChange={(e) => setEmail(e.target.value)} />
           </div>
           <div className="my-3">
-              <label htmlFor="password" className="form-label">Password</label>
-              <input type="text" name="password" id="password" className="form-control" />
+            <label htmlFor="password" className="form-label">Password</label>
+            <input type="password" name="password" id="password" className="form-control" onChange={(e) => setPassword(e.target.value)} />
           </div>
           <div className="my-5">
-              <div className="loginBtn my-4">
-                <button className='btn' onClick={handleSubmit}>Log In</button>
-              </div>
-              <hr />
-              <div className="toSignupBtn my-4">
-                <Link to="/signup">Sign Up</Link>
-              </div>
+            <div className="loginBtn my-4">
+              <button className='btn' onClick={handleSubmit}>Log In</button>
+            </div>
+            <hr />
+            <div className="toSignupBtn my-4">
+              <Link to="/signup">Sign Up</Link>
+            </div>
           </div>
         </form>
       </div>
     </div>
-  )
+  );
 }
-

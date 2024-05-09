959f44c181cb8b80052a761e0ceea5daa976a537
Aimane Jaghda <aimane.jaghda@gmail.com>
2024-05-09 11:37:31 +0100

    working on signup page

---
+++ frontend/src/components/BrandLogo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function BrandLogo() {
+  return (
+    <div>
+      <div className="brand-logo text-center my-3">
+          <span className='display-3'>ABS</span> center
+        </div>
+    </div>
+  )
+}

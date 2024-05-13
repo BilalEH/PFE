6105f206b13de997d145453def9f2a305d3f7018
Aimane Jaghda <aimane.jaghda@gmail.com>
2024-05-13 10:46:29 +0100

    creating admin dashboard

--- frontend/src/layouts/AdminLayout.jsx
+++ frontend/src/layouts/AdminLayout.jsx
@@ -1,11 +1,34 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import "./style/adminLayout.css"
+import { Avatar } from "@mui/material";
+import useAuthContext from "../api/auth";
+
 
 export default function AdminLayout() {
+    const { importUser } = useAuthContext();
     return (
         <>
-            <header>{/* <Heading></Heading> */}</header>
-            <main>
+            <div className="row">
+                <div className="dashboard pe-0">
+                    <div className="dashElement p-3">
+                        <Link to="/profile">
+                            <div className="d-flex">
+                                <div>
+                                Profile
+                            </div>
+                            <div className="ms-auto">
+                                <Avatar alt={`${importUser().firstName} ${importUser().lastName}`} src={importUser().avatar} />
+                            </div>
+                            </div>
+                        </Link>
+                    </div>
+                    <div className="dashElement p-3">
+                        Saad dirassa
+                    </div>
+                </div>
+            </div>
+            <main className="adminPages">
                 <Outlet></Outlet>
             </main>
         </>

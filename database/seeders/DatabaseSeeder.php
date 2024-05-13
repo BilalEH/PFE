d9504200e44d14723a2dca77cbeebfa8e050666a
Aimane Jaghda <aimane.jaghda@gmail.com>
2024-05-13 23:04:59 +0100

    making dashboard for admin and adding routes and components for it

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
             'firstName' => "Aimane",
             'lastName' => "Jaghda",
             'role' => 'admin',
-            'avatar' => "https://ui-avatars.com/api/?uppercase=false&name=aimane+jaghda",
+            'avatar' => "https://ui-avatars.com/api/?uppercase=false&name=Aimane+Jaghda",
             'email' => 'Aimane@gmail.com',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password,
         ]);
---
+++ frontend/src/pages/admin/AdminMessages.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function AdminMessages() {
+  return (
+    <>
+        <div>
+            Messages
+        </div>
+    </>
+  )
+}

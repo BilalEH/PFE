17fdcb53a448672c5fbfe01c91df648caafad81a
Bilal <sfsdfiuwhef@gmail.com>
2024-05-16 15:12:22 +0100

    v2 to profiles pages update

--- frontend/src/pages/admin/AdminProfile.jsx
+++ frontend/src/pages/admin/AdminProfile.jsx
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import useAuthContext from '../../api/auth';
 import { useEffect } from 'react';
 import { GetAdmins } from '../../api/adminsStore/adminStore';
+import { CircularProgress } from '@mui/material';
 
 function AdminProfile() {
   const dispatch = useDispatch();
@@ -10,23 +11,24 @@ function AdminProfile() {
     dispatch(GetAdmins());
   },[])
   const userData = useSelector((state) => state.admins);
-  console.log(userData);
-  if (userData.status!='succeeded') {
-    return <div>No admin data available</div>;
-  }
   const user=userData.admins.find((e)=>e.user_id.id==importUser().id)
   return (
     <div>
       <h2>Admin Profile</h2>
-      <div>
-        <p><strong>lastName:</strong> {user.user_id.lastName}</p>
-        <p><strong>firstName:</strong> {user.user_id.firstName}</p>
-        <p><strong>cin:</strong> {user.user_id.cin}</p>
-        <p><strong>Email:</strong> {user.user_id.email}</p>
-        <p><strong>phone:</strong> {user.user_id.phone}</p>
-        <p><strong>role:</strong> {user.user_id.role}</p>
-      </div>
-    </div>
+      {
+        userData.status==='succeeded'?(
+          <div>
+            <p><strong>lastName:</strong> {user.user_id.lastName}</p>
+            <p><strong>firstName:</strong> {user.user_id.firstName}</p>
+            <p><strong>cin:</strong> {user.user_id.cin}</p>
+            <p><strong>Email:</strong> {user.user_id.email}</p>
+            <p><strong>phone:</strong> {user.user_id.phone}</p>
+            <p><strong>role:</strong> {user.user_id.role}</p>
+            <p><strong>Status:</strong> {user.status==0?'Not verified':'Verified'}</p>
+          </div>
+        ):(userData.status==='failed'?(<div className='w-100 d-flex justify-content-center'>No admin data available</div>):(<div className='w-100 d-flex justify-content-center'><CircularProgress/></div>))
+      }
+</div>
   );
 }
 

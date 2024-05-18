bfb5d003f322232dfc6e5673edb3ebb24a5213f8
Aimane Jaghda <aimane.jaghda@gmail.com>
2024-05-18 23:06:40 +0100

    making a static messages page for admin

--- frontend/src/pages/admin/AdminMessages/AdminMessages.jsx
+++ frontend/src/pages/admin/AdminMessages/AdminMessages.jsx
@@ -1,11 +1,130 @@
-import React from 'react'
+import React from "react";
+import "./style/AdminMessages.css";
+import Message from "./components/Message";
 
 export default function AdminMessages() {
-  return (
-    <>
-        <div>
-            Messages
-        </div>
-    </>
-  )
+    return (
+        <>
+            <div>
+                <div className="page-title">Messages</div>
+                <div className="messages-container row">
+                    <div className="col-4 p-0 messagers-container">
+                        <div className="msger">
+                            <div className="msger-name d-flex align-items-center justify-content-">
+                                <div>Saad Dirassa</div>
+                                <span className="msger-role rounded-pill">
+                                    Modris
+                                </span>
+                            </div>
+                            <div className="msg-title text-muted">
+                                Message Title
+                            </div>
+                        </div>
+                        <div className="msger">
+                            <div className="msger-name d-flex align-items-center justify-content-">
+                                <div>Saad Dirassa</div>
+                                <span className="msger-role rounded-pill">
+                                    Modris
+                                </span>
+                            </div>
+                            <div className="msg-title text-muted">
+                                Message Title
+                            </div>
+                        </div>
+                        <div className="msger">
+                            <div className="msger-name d-flex align-items-center justify-content-">
+                                <div>Saad Dirassa</div>
+                                <span className="msger-role rounded-pill">
+                                    Modris
+                                </span>
+                            </div>
+                            <div className="msg-title text-muted">
+                                Message Title
+                            </div>
+                        </div>
+                        <div className="msger">
+                            <div className="msger-name d-flex align-items-center justify-content-">
+                                <div>Saad Dirassa</div>
+                                <span className="msger-role rounded-pill">
+                                    Modris
+                                </span>
+                            </div>
+                            <div className="msg-title text-muted">
+                                Message Title
+                            </div>
+                        </div>
+                        <div className="msger">
+                            <div className="msger-name d-flex align-items-center justify-content-">
+                                <div>Saad Dirassa</div>
+                                <span className="msger-role rounded-pill">
+                                    Modris
+                                </span>
+                            </div>
+                            <div className="msg-title text-muted">
+                                Message Title
+                            </div>
+                        </div>
+                        <div className="msger">
+                            <div className="msger-name d-flex align-items-center justify-content-">
+                                <div>Saad Dirassa</div>
+                                <span className="msger-role rounded-pill">
+                                    Modris
+                                </span>
+                            </div>
+                            <div className="msg-title text-muted">
+                                Message Title
+                            </div>
+                        </div>
+                        <div className="msger">
+                            <div className="msger-name d-flex align-items-center justify-content-">
+                                <div>Saad Dirassa</div>
+                                <span className="msger-role rounded-pill">
+                                    Modris
+                                </span>
+                            </div>
+                            <div className="msg-title text-muted">
+                                Message Title
+                            </div>
+                        </div>
+                        <div className="msger">
+                            <div className="msger-name d-flex align-items-center justify-content-">
+                                <div>Saad Dirassa</div>
+                                <span className="msger-role rounded-pill">
+                                    Modris
+                                </span>
+                            </div>
+                            <div className="msg-title text-muted">
+                                Message Title
+                            </div>
+                        </div>
+                        <div className="msger">
+                            <div className="msger-name d-flex align-items-center justify-content-">
+                                <div>Saad Dirassa</div>
+                                <span className="msger-role rounded-pill">
+                                    Modris
+                                </span>
+                            </div>
+                            <div className="msg-title text-muted">
+                                Message Title
+                            </div>
+                        </div>
+                        <div className="msger">
+                            <div className="msger-name d-flex align-items-center justify-content-">
+                                <div>Saad Dirassa</div>
+                                <span className="msger-role rounded-pill">
+                                    Modris
+                                </span>
+                            </div>
+                            <div className="msg-title text-muted">
+                                Message Title
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-8 msg-container">
+                        <Message />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }

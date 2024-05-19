4750b9eb7ab53ea576dfbcfec6424bf0c941df53
Aimane Jaghda <aimane.jaghda@gmail.com>
2024-05-19 23:04:33 +0100

    empty messages output for adminMessages

--- frontend/src/pages/admin/AdminMessages/AdminMessages.jsx
+++ frontend/src/pages/admin/AdminMessages/AdminMessages.jsx
@@ -1,11 +1,12 @@
 import "./style/AdminMessages.css";
 import Message from "./components/Message";
-import { useEffect,useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 import { GetUserMessages } from "../../../api/adminsStore/adminStore";
 import { CircularProgress } from "@mui/material";
 import EmptyMessage from "./components/EmptyMessage";
+import EmptyMessagesHandle from "./components/EmptyMessagesHandle";
 
 export default function AdminMessages() {
     const [messageSelected, setMessageSelected] = useState("none");
@@ -17,33 +18,43 @@ export default function AdminMessages() {
 
     const MessagesData = useSelector((state) => state.admins);
     // console.log(MessagesData);
-
+    console.log(MessagesData.messages.length);
     return (
         <>
             <div>
                 <div className="page-title">Messages</div>
                 <div className="messages-container row">
                     <div className="col-4 p-0 messagers-container">
-                        {
-                            MessagesData.status_message==='succeeded'?(
-                                MessagesData.messages.map(e=>{
-                                    return(
+                        {MessagesData.status_message === "succeeded" ? (
+                            MessagesData.length === 0 ? (
+                                <EmptyMessagesHandle />
+                            ) : (
+                                MessagesData.messages.map((e) => {
+                                    return (
                                         <div className="msger" key={e.id}>
-                                            <div className="msger-name d-flex align-items-center justify-content-">
-                                                <div>
-                                                    {e.user_id.firstName}{" "}
-                                                    {e.user_id.lastName}
+                                            <button
+                                                onClick={() =>
+                                                    setMessageSelected(e)
+                                                }
+                                                className="msger-button"
+                                            >
+                                                <div className="msger-name d-flex align-items-center justify-content-">
+                                                    <div>
+                                                        {e.user_id.firstName}{" "}
+                                                        {e.user_id.lastName}
+                                                    </div>
+                                                    <span className="msger-role rounded-pill">
+                                                        {e.user_id.role}
+                                                    </span>
                                                 </div>
-                                                <span className="msger-role rounded-pill">
-                                                    {e.user_id.role}
-                                                </span>
-                                            </div>
-                                            <div className="msg-title text-muted">
-                                                {e.title}
-                                            </div>
+                                                <div className="msg-title text-muted">
+                                                    {e.title}
+                                                </div>
+                                            </button>
                                         </div>
                                     );
-                            })
+                                })
+                            )
                         ) : MessagesData.status_message === "failed" ? (
                             <div className="w-100 h-100 d-flex justify-content-center">
                                 No messages available
